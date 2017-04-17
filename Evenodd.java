3343d3ee8c4026b0ed9a71d344fbe380c617efa2
vanapreethi <vanapreethimurugesan@gmail.com>
2017-04-17 14:13:31 +0530

    Update and rename dgdgrd.c to Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Evenodd
+  {
+public static void main(String args[])
+  {
+  Scanner number=new Scanner(System.in);
+  System.out.println("enter number");
+  int s=number.nextInt();
+  if(s%2==0)
+  {
+      System.out.println("given number is Even");
+  }
+  else
+       System.out.println("given number is odd");
+  }}
+
